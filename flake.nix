86a5b7e08bf6454b29f80448c10b7a80709b94f5
Cole Helbling <cole.e.helbling@outlook.com>
2020-08-27 00:39:01 -0700

    Add support for Beta and Stable

--- flake.nix
+++ flake.nix
@@ -12,7 +12,9 @@
 
   outputs = inputs:
     let
-      metadata = builtins.fromJSON (builtins.readFile ./latest.json);
+      inherit (sysPkgs.lib.firefoxOverlay) firefox_versions;
+
+      metadata = version: builtins.fromJSON (builtins.readFile (./. + "/${version}.json"));
 
       nameValuePair = name: value: { inherit name value; };
       genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
@@ -27,59 +29,97 @@
 
       # impure, but that's by design
       sysPkgs = (pkgsFor inputs.nixpkgs builtins.currentSystem);
-      version = {
-        name = "Firefox Nightly";
-        version = sysPkgs.lib.firefoxOverlay.firefox_versions.FIREFOX_NIGHTLY;
-        release = false;
-      };
 
       variants = system: {
         firefox-nightly-bin =
+          let
+            meta = metadata "nightly";
+          in
           (pkgsFor inputs.nixpkgs system).lib.firefoxOverlay.firefoxVersion (
-            metadata.version // { info = metadata.cachedInfo; }
+            meta.version // { info = meta.cachedInfo; }
+          );
+        firefox-beta-bin =
+          let
+            meta = metadata "beta";
+          in
+          (pkgsFor inputs.nixpkgs system).lib.firefoxOverlay.firefoxVersion (
+            meta.version // { info = meta.cachedInfo; }
+          );
+        firefox-bin =
+          let
+            meta = metadata "stable";
+          in
+          (pkgsFor inputs.nixpkgs system).lib.firefoxOverlay.firefoxVersion (
+            meta.version // { info = meta.cachedInfo; }
           );
       };
     in
     rec {
       devShell =
-       forAllSystems (system:
-        let 
-          master_ = pkgsFor inputs.master system;
-          nixpkgs_ = pkgsFor inputs.nixpkgs system;
-          cachixpkgs_ = pkgsFor inputs.cachixpkgs system;
-        in nixpkgs_.mkShell {
-          nativeBuildInputs = with nixpkgs_; [
-            bash cacert curl git jq openssh ripgrep
-            cachixpkgs_.cachix
-            master_.nixFlakes
-            master_.nix-build-uncached
-          ];
-        }
-      );
+        forAllSystems (system:
+          let
+            master_ = pkgsFor inputs.master system;
+            nixpkgs_ = pkgsFor inputs.nixpkgs system;
+            cachixpkgs_ = pkgsFor inputs.cachixpkgs system;
+          in
+          nixpkgs_.mkShell {
+            nativeBuildInputs = with nixpkgs_; [
+              bash
+              cacert
+              curl
+              git
+              jq
+              openssh
+              ripgrep
+              cachixpkgs_.cachix
+              master_.nixFlakes
+              master_.nix-build-uncached
+            ];
+          }
+        );
 
       packages = forAllSystems (system:
         let
           nixpkgs_ = (pkgsFor inputs.nixpkgs system);
           attrValues = inputs.nixpkgs.lib.attrValues;
-        in (variants system)
+        in
+        (variants system)
       );
 
-      latest =
+      latest = version:
         let
           pkgs = pkgsFor inputs.nixpkgs builtins.currentSystem;
           cachedInfo = pkgs.lib.firefoxOverlay.versionInfo version;
         in
         { inherit version cachedInfo; };
 
-      defaultPackage = forAllSystems (system:
-        let
-          nixpkgs_ = (pkgsFor inputs.nixpkgs system);
-          attrValues = inputs.nixpkgs.lib.attrValues;
-        in
-        nixpkgs_.symlinkJoin {
-          name = "flake-firefox-nightly";
-          paths = attrValues (variants system);
-        }
-      );
+      nightly = latest {
+        name = "Firefox Nightly";
+        version = firefox_versions.FIREFOX_NIGHTLY;
+        release = false;
+      };
+
+      beta = latest {
+        name = "Firefox Beta";
+        version = firefox_versions.LATEST_FIREFOX_DEVEL_VERSION;
+        release = true;
+      };
+
+      stable = latest {
+        name = "Firefox";
+        version = firefox_versions.LATEST_FIREFOX_VERSION;
+        release = true;
+      };
+
+      # defaultPackage = forAllSystems (system:
+      #   let
+      #     nixpkgs_ = (pkgsFor inputs.nixpkgs system);
+      #     attrValues = inputs.nixpkgs.lib.attrValues;
+      #   in
+      #   nixpkgs_.symlinkJoin {
+      #     name = "flake-firefox-nightly";
+      #     paths = attrValues (variants system);
+      #   }
+      # );
     };
 }
--- update.sh
+++ update.sh
@@ -1,43 +1,61 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
 set -euo pipefail
 set -x
 
 cache="nixpkgs-wayland"
 
-oldversion="$(cat latest.json | jq -r '.cachedInfo.chksum' |  grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}')"
-rm -rf ./.ci/commit-message
+for variant in nightly beta stable; do
 
-nix flake --experimental-features 'nix-command flakes' \
-  update \
-    --update-input master \
-    --update-input nixpkgs \
-    --update-input mozilla \
-    --update-input cachixpkgs
+  if [ "${variant}" = "nightly" ]; then
+    oldversion="$(jq -r '.cachedInfo.chksum' < ${variant}.json | grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}')"
+  else
+    oldversion="$(jq -r '.version.version' < ${variant}.json)"
+  fi
 
-nix --experimental-features 'nix-command flakes' \
-  eval --impure '.#latest' --json \
-    | jq > latest.json
+  rm -rf ./.ci/commit-message
 
-newversion="$(cat latest.json | jq -r '.cachedInfo.chksum' |  grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}')"
+  nix flake --experimental-features 'nix-command flakes' \
+    update \
+      --update-input master \
+      --update-input nixpkgs \
+      --update-input mozilla \
+      --update-input cachixpkgs
 
-out="$(mktemp -d)"
-nix-build-uncached \
-  --option "extra-binary-caches" "https://cache.nixos.org https://nixpkgs-wayland.cachix.org" \
-  --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
-  --option "build-cores" "0" \
-  --option "narinfo-cache-negative-ttl" "0" \
-  --out-link "${out}/result" packages.nix
-results=(); shopt -s nullglob
-for f in ${out}/result*; do
-  results=("${results[@]}" "${f}")
+  nix --experimental-features 'nix-command flakes' \
+    eval --impure ".#${variant}" --json \
+      | jq > ${variant}.json
+
+  if [ "${variant}" = "nightly" ]; then
+    newversion="$(jq -r '.cachedInfo.chksum' < ${variant}.json | grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}')"
+  else
+    newversion="$(jq -r '.version.version' < ${variant}.json)"
+  fi
+
+  out="$(mktemp -d)"
+  nix-build-uncached \
+    --option "extra-binary-caches" "https://cache.nixos.org https://nixpkgs-wayland.cachix.org" \
+    --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
+    --option "build-cores" "0" \
+    --option "narinfo-cache-negative-ttl" "0" \
+    --out-link "${out}/result" packages.nix
+
+  results=(); shopt -s nullglob
+  for f in "${out}"/result*; do
+    results=("${results[@]}" "${f}")
+  done
+
+  echo "${results[@]}" | cachix push "${cache}"
+
+  if [[ "${newversion}" != "${oldversion}" ]]; then
+    commitmsg="${variant}: ${oldversion} -> ${newversion}"
+    echo -e "${commitmsg}" > .ci/commit-message
+    # TODO: commit here, per-variant
+  else
+    echo "nothing to do, there was no version bump"
+  fi
+
 done
-echo "${results[@]}" | cachix push "${cache}"
-
-if [[ "${newversion}" != "${oldversion}" ]]; then
-  commitmsg="firefox-nightly-bin: ${oldversion} -> ${newversion}"
-  echo -e "${commitmsg}" > .ci/commit-message
-else
-  echo "nothing to do, there was no version bump"
-fi
